dace9c23455294eb770798b0ef07dc9004bf84ee
Jean-AT <jeanpoolariass2020@hotmail.com>
2026-02-05 22:28:30 -0500

    Add login v0.1

--- CommandServiceFront/src/app/app.ts
+++ CommandServiceFront/src/app/app.ts
@@ -1,11 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ProductCart } from './shared/product-cart/product-cart';
-import { NavBar } from './shared/nav-bar/nav-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,ProductCart,NavBar],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
